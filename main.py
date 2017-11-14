5e3679eeb561b5143b2ae018f18512b58b70e549
Marcel <m.rudnicki84@gmail.com>
2017-11-14 21:57:48 +0100

    Add 'Add button' functionality

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ Last Name, Email, Phone. Additionally each database entry will get a
 unique ID in order to distinguish between entries.
 """
 import wx
+import random
 
 
 class PeopleDatabase(wx.Frame):
@@ -17,6 +18,7 @@ class PeopleDatabase(wx.Frame):
     def __init__(self, *args, **kw):
         # ensure the parent's __init__ is called
         super(PeopleDatabase, self).__init__(*args, **kw)
+        self._people = {}
 
         # create a panel in the frame
         self._pnl = wx.Panel(self)
@@ -25,7 +27,12 @@ class PeopleDatabase(wx.Frame):
         self._peopleStaticBox = wx.StaticBox(self._pnl, wx.ID_ANY, "People",
                                              size=wx.Size(200, 250))
         # people list
-        self._peopleList = wx.ListCtrl(self._pnl, size=wx.Size(200, 250))
+        self._peopleList = wx.ListCtrl(self._pnl, size=wx.Size(190, 225),
+                                       pos=wx.Point(5, 20),
+                                       style=wx.LC_REPORT)
+        self._peopleList.InsertColumn(0, ' First Name')
+        self._peopleList.InsertColumn(1, ' Last Name')
+        self._peopleList.Arrange()
 
         # create Person Details area
         self._personDetBox = wx.StaticBox(self._pnl, wx.ID_ANY, "Person Details",
@@ -58,15 +65,20 @@ class PeopleDatabase(wx.Frame):
 
         # Delete button
         self._deleteButton = wx.Button(self._pnl, label="Delete", pos=(0, 255))
+        # TODO: BUG - Delete button should be disabled (grayed out)
+        # when no item is chosen from the People list. Also when
+        # starting the application
 
         # Add button
         self._addButton = wx.Button(self._pnl, label="Add", pos=(210, 255))
 
         # Update button
         self._updateButton = wx.Button(self._pnl, label="Update", pos=(300, 255))
+        self._updateButton.Disable()
 
         # Save button
         self._saveButton = wx.Button(self._pnl, label="Save", pos=(430, 255))
+        self._saveButton.Disable()
 
         # Load button
         self._loadButton = wx.Button(self._pnl, label="Load", pos=(520, 255))
@@ -86,7 +98,12 @@ class PeopleDatabase(wx.Frame):
         pass
 
     def on_add_button(self, event):
-        pass
+        person_id = self.generate_id()
+        self._people[person_id] = {'FirstName': "Name", 'LastName': "Surname"}
+
+        self.set_person_details_fields(self._people[person_id], person_id)
+        self.add_person_to_list(self._people[person_id]['FirstName'],
+                                self._people[person_id]['LastName'])
 
     def on_update_button(self, event):
         pass
@@ -97,6 +114,28 @@ class PeopleDatabase(wx.Frame):
     def on_load_button(self, event):
         pass
 
+    def add_person_to_list(self, firstName, lastName):
+        self._peopleList.Append([firstName, lastName])
+
+    def set_person_details_fields(self, person, personId):
+        self._idEditId.SetLabelText(str(personId))
+        if 'FirstName' in person:
+            self._firstNameEditId.SetLabelText(person['FirstName'])
+        if 'MiddleName' in person:
+            self._middleNameEditId.SetLabelText(person['MiddleName'])
+        if 'LastName' in person:
+            self._lastNameEditId.SetLabelText(person['LastName'])
+        if 'Email' in person:
+            self._emailEditId.SetLabelText(person['Email'])
+        if 'Phone' in person:
+            self._phoneEditId.SetLabelText(person['Phone'])
+
+    def generate_id(self):
+        while True:
+            num = random.randrange(0, 99999999)
+            if num not in self._people.keys():
+                return num
+
 if __name__ == '__main__':
     # When this module is run (not imported) then create the app, the
     # frame, show it, and start the event loop.
@@ -104,3 +143,4 @@ if __name__ == '__main__':
     frm = PeopleDatabase(None, title='peopleFinder', size=wx.Size(627, 350))
     frm.Show()
     app.MainLoop()
+
